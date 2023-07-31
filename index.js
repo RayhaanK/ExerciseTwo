ff3246144d20c7bcef0549078bfe7200445409c2
RayhaanK <rayhaankhan331@gmail.com>
2023-07-31 15:48:10 +0200

    added exercise

---
+++ index.js
@@ -0,0 +1,9 @@
+const Person = require('./person')
+
+const person1 = new Person('Rayhaan', 'Khan', 'rayhaankhan04@gmail.com')
+const person2 = new Person('Nuraan', 'Samodien', 'nuraansa1@gmail.com')
+const person3 = new Person('Ganiefah', 'Majiet', 'majiet180804@gmail.com')
+
+console.log(person1);
+console.log(person2);
+console.log(person3);
